3e8c76242c45b4f91c5ab5f34181d97a340b906b
Rebecca <rebecca.fernands@gmail.com>
2019-09-13 16:51:32 -0300

    commit inicial

---
+++ app/src/main/java/com/example/rebecca/rec_mobile_ii/MealsListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rebecca.rec_mobile_ii
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MealsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meals_list)
+    }
+}
